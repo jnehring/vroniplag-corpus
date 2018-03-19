c4bc33b74500ff1de50c292ba404b43226ca6ce5
bgrusdt <grusdt@uni-potsdam.de>
2018-03-19 15:07:47 +0100

    updated readme

--- src/main/java/de/jn/paraphrases/Test.java
+++ src/main/java/de/jn/paraphrases/Test.java
@@ -1,15 +1,22 @@
 package de.jn.paraphrases;
 
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
 
 /**
  * Created by jan on 12.09.17.
  */
 public class Test {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
 //        String html = "<td>abcd<br/>bla<hr>bla</td>";
 //        int index = html.toLowerCase().indexOf("<hr>");
 //        System.err.println(html);
@@ -20,14 +27,30 @@ public class Test {
 //        System.err.println(doc.text());
     	
     	
-    	List<String> p = Arrays.asList("a", "b", "c", "d", "e");
-    	List<String> s = Arrays.asList("a", "c", "e", "g");
+//    	List<String> p = Arrays.asList("a", "b", "c", "d", "e");
+//    	List<String> s = Arrays.asList("a", "c", "e", "g");
+//    	
+//    	List<String> unavailable = p.stream()
+//                .filter(e -> s.contains(e))
+//                .collect(Collectors.toList());
+//    	
+//    	System.out.println(unavailable);
     	
-    	List<String> unavailable = p.stream()
-                .filter(e -> s.contains(e))
-                .collect(Collectors.toList());
     	
-    	System.out.println(unavailable);
+    	// STANFORD CORENLP
+//        Properties props = new Properties();
+//        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+//        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+//
+//        // read some text in the text variable
+//        String text = "...";
+//
+//        // create an empty Annotation just with the given text
+//        Annotation document = new Annotation(text);
+//
+//        // run all Annotators on this text
+//        pipeline.annotate(document);
+
     	
-    }
+}
 }
